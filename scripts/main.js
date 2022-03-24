877e3c092623442ade78eb6940a672261222fc73
marcos-ribas25 <marcosvitor530@gmail.com>
2022-03-24 08:21:13 -0300

    js update

--- scripts/main.js
+++ scripts/main.js
@@ -57,32 +57,6 @@ $(document).ready(function () {
    }
 })
 
-
-
-
-   if ($('.cep-mask').length > 0) {
-      $('.cep-mask').mask('99.999-999');
-   }
-
-   if ($('.date-mask').length > 0) {
-      $('.date-mask').mask('99/99/9999');
-   }
-
-   if ($('.number-mask').length > 0) {
-      $('.number-mask').mask('99999');
-   }
-
-   if ($('.cvv-mask').length > 0) {
-      $('.cvv-mask').mask('999');
-   }
-
-   if ($('.cpf-mask').length > 0) {
-      $('.cpf-mask').mask('999.999.999-99');
-   }
-
-   if ($('.phone-mask').length > 0) {
-      $('.phone-mask').mask(SPMaskBehavior, spOptions);
-   }
 });
 
 new PureCounter();
